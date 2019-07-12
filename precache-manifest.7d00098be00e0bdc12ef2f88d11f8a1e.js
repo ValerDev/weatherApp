dd496773955dc3da34e01df2dff833dc2017a80c
Val <50718596+ValerDev@users.noreply.github.com>
2019-07-13 02:01:35 +0300

    Add files via upload

---
+++ precache-manifest.7d00098be00e0bdc12ef2f88d11f8a1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931a53d842656c204abd21aac6fd0cb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60c852e014bb3158578c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e22732c1013f7c365b28",
+    "url": "/static/css/main.456b1e78.chunk.css"
+  },
+  {
+    "revision": "60c852e014bb3158578c",
+    "url": "/static/js/2.40a8b625.chunk.js"
+  },
+  {
+    "revision": "e22732c1013f7c365b28",
+    "url": "/static/js/main.e033d623.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/static/media/polygon.cc54c19b.jpg"
+  }
+]);
